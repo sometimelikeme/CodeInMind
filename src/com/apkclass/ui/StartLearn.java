57c19c60571c97c51912ede0da8afdba26ee6a80
jiangkun <jiangkunwork@126.com>
2014-11-03 13:59:32 +0800

    2014.11.03

--- src/com/apkclass/ui/StartLearn.java
+++ src/com/apkclass/ui/StartLearn.java
@@ -6,11 +6,16 @@ import java.util.Map;
 import com.apkclass.adapter.DocsListAdapter;
 import com.apkclass.study.CodeBean;
 import com.apkclass.study.CodeProvider;
+import com.apkclass.study.LearnPage;
 import com.apkclass.utils.log;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -34,6 +39,18 @@ public class StartLearn extends Activity {
 		contentlistadapter = new DocsListAdapter(StartLearn.this);
 		contentlistadapter.setDatalist(titlelist);
 		ll_contentlist.setAdapter(contentlistadapter);
+		
+		ll_contentlist.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(StartLearn.this, LearnPage.class);
+				intent.putExtra("", position);
+				startActivity(intent);
+			}
+		});
 		
 
 		
