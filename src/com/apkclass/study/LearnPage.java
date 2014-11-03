169780ad2a2ea7efe99732cec1835fdb076b4349
jiangkun <jiangkunwork@126.com>
2014-11-03 13:59:32 +0800

    2014.11.03

---
+++ src/com/apkclass/study/LearnPage.java
@@ -0,0 +1,18 @@
+package com.apkclass.study;
+
+
+import com.apkclass.ui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LearnPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.learnpage);
+	}
+	
+
+}
